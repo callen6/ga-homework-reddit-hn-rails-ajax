6e37e54fea4af73263899ef3a008d91baf478dcc
Cori Allen <coriallen@gmail.com>
2013-11-07 19:29:09 -0500

    Add gem pry to Gemfile.

---
+++ hacker_news_clone/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackerNewsClone::Application.config.secret_key_base = '8e16276dc1ed0e5671aae6d1a1580b7e5ff983ebce2663823cace601f6fa38415dce58561bb88ac42e5a14a9ef4020b58a8785686ae414abb254470393ab638d'
